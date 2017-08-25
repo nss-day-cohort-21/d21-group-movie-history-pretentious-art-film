a14097d868f404ed60e24924a9ec812901d954cc
AustinJ <austin.jay.kurtis@gmail.com>
2017-08-25 10:21:13 -0500

    added modal and stars

--- js/main.js
+++ js/main.js
@@ -3,3 +3,16 @@
 const $ = require('jquery');
 
 console.log('From Main');
+
+
+//*************************
+//		Star Rating
+//*************************
+
+$(function () {
+ 
+  $("#rateYo").rateYo({
+    rating: 0,
+    fullStar: true
+  });
+});
