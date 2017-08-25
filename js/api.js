14a73865c260d5d942063ce8364ef74f226a61a6
Jeremy Sheridan Wells <jsheridanwells@gmail.com>
2017-08-25 08:39:31 -0500

    fixed api security issue

--- js/api.js
+++ js/api.js
@@ -7,16 +7,3 @@ function getAPI() {
 }
 
 module.exports = getAPI;
-
-
-// let api=  'e2e66be55b4a3705a80063c2788179a1';
-
-// let Api = {
-// 	getTMDBkey: () => {
-// 		console.log("key", api);
-// 		return api;
-// 	}
-// };
-
-// Api.getTMDBkey();
-// module.exports = Api;
