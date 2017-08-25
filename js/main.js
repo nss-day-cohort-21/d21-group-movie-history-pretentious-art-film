f56e8184b2f1fa8d333547775bcdc5c44b05ca9a
AustinJ <austin.jay.kurtis@gmail.com>
2017-08-25 15:01:14 -0500

    added stars and CSS for index

--- js/main.js
+++ js/main.js
@@ -1,19 +1,7 @@
 'use strict';
 
 const $ = require('jquery');
-
 console.log('From Main');
 
 
-//*************************
-//		Star Rating
-//*************************
-
-$(function () {
- 
-  $("#rateYo").rateYo({
-    rating: 0,
-    fullStar: true
-  });
-});
 
