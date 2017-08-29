f863ed128c0e697df77fe2980e8ed3f669cb6ef6
Fang <fang-wen.shen@vanderbilt.edu>
2017-08-29 10:42:27 -0500

    nothing

---
+++ js/deletemovie.js
@@ -0,0 +1,14 @@
+// $(`#${deleteMovie}`).on("click", (event)=>{
+
+					
+// 			    	$.ajax({
+// 				      url: `https://musichistory-43b58.firebaseio.com/${newSong.key}/.json`,
+// 				      method: "DELETE"
+// 				    })
+// 				    .done(function(response) {
+// 				    	deleteself(event);	
+// 				      // console.log("response after deletion", response);
+// 				      // You'll likely want to execute the code that you're using
+// 				      // on page load here to run the GET XHR and bind to Handlebars
+// 				    });
+// 				});	
